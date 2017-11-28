e6e0eccba5792164142e8e25056024278d7b9d0e
Pedro Barco <pedro.barco@tecnico.ulisboa.pt>
2017-11-28 22:06:21 +0000

    Add detached app

---
+++ android/app/src/main/java/io/ist/gwarant/generated/ExponentBuildConstants.java
@@ -0,0 +1,8 @@
+package io.ist.gwarant.generated;
+
+// This file is auto-generated. Please don't rename!
+public class ExponentBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "";
+
+}
