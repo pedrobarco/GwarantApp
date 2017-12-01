8722cbb04c94570235cb15cb7c5183f947c1ddaf
Barco <pedro.barco@tecnico.ulisboa.pt>
2017-12-01 14:55:34 +0000

    Handle key gen and storage

--- screens/MainScreen/index.js
+++ screens/MainScreen/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { AppLoading, SecureStore } from 'expo';
 import { View, Text } from 'react-native';
 import { Button } from 'react-native-elements';
 import { styles } from '../../config/styles';
 
 export default class MainScreen extends React.Component {
+  state = {
+    isReady: false,
+    key: null,
+  };
+
   render() {
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._manageKeysAsync}
+          onFinish={this._handleFinishLoadingAsync}
+        />
+      );
+    }
     return (
       <View style={styles.container}>
         <View style={styles.titleContainer}>
@@ -16,17 +30,55 @@ export default class MainScreen extends React.Component {
           icon={{ name: 'fingerprint' }}
           title='REGISTER'
           buttonStyle={styles.button}
-          onPress={() => this.props.navigation.navigate('Scanner')} />
+          onPress={() => this.props.navigation.navigate('Scanner')}
+        />
         <Button
           large
           icon={{ name: 'cast-connected' }}
           title='CONNECT'
-          buttonStyle={styles.button} />
+          buttonStyle={styles.button}
+          onPress={() => alert(this.state.key.pub)}
+        />
       </View>
     );
+  }
+
+  _manageKeysAsync = async () => {
+    let key = {
+      pub: await SecureStore.getItemAsync("key.pub"),
+      priv: await SecureStore.getItemAsync("key.priv")
+    }
+    if (key.pub == null || key.priv == null) {
+      const rsa = genKeys()
+      key = {
+        pub: rsa.getPublicString(),
+        priv: rsa.getPrivateString()
+      }
+      await SecureStore.setItemAsync("key.pub", key.pub)
+      await SecureStore.setItemAsync("key.priv", key.priv)
+    }
+  }
+
+  _handleFinishLoadingAsync = async () => {
+    this.setState({
+      isReady: true,
+      key: {
+        pub: await SecureStore.getItemAsync("key.pub"),
+        priv: await SecureStore.getItemAsync("key.priv")
+      }
+    })
   }
 }
 
 MainScreen.propTypes = {
   navigation: PropTypes.any.isRequired
 };
+
+function genKeys() {
+  const RSAKey = require('react-native-rsa')
+  const bits = 512
+  const exponent = '10001' // must be a string. This is hex string. decimal = 65537
+  let rsa = new RSAKey()
+  rsa.generate(bits, exponent)
+  return rsa
+}
