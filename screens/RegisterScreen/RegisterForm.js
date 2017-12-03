7069586287d5faf21ceed0b54b9569d8c7782454
Barco <pedro.barco@tecnico.ulisboa.pt>
2017-12-03 23:19:31 +0000

    Fix message encryption encoding

--- screens/RegisterScreen/RegisterForm.js
+++ screens/RegisterScreen/RegisterForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { SecureStore } from 'expo';
 import { View, TextInput } from 'react-native';
 import { Button } from 'react-native-elements';
 import { colors, styles } from '../../config/styles';
@@ -32,26 +33,28 @@ RegisterForm.propTypes = {
   qrcode: PropTypes.array.isRequired
 };
 
-function check(qrcode, password) {
+async function check(qrcode, password) {
   const crypto = require('crypto-js')
   const receivedhash = qrcode[1]
   const salt = qrcode[2]
   const ip = qrcode[4]
-  const calculatedhash = crypto.PBKDF2(password, salt, {hasher: crypto.algo.SHA256,  keySize: 256 / 32, iterations: 10000})
-  // if (receivedhash == calculatedhash) { // TODO: Easier for testing
-  const key = crypto.PBKDF2(password, salt, {hasher: crypto.algo.SHA256,  keySize: 256 / 24, iterations: 10000}) // Not sure if keysize is correct
-  const message = "ID Smartphone" + " " + "Public Key Smartphone" // TODO: put a unique id here and the Pubkey
-  const cryptedMessage = crypto.AES.encrypt(message, key) // No IV { iv: iv }
-  alert("Making connection with ws://" + ip + ':8080/')
-  const ws = new WebSocket('ws://' + ip + ':8080/')
-  ws.onopen = () => {
-    ws.send(cryptedMessage)
-    ws.onmessage = e => {
-      // Receive ack
-      // Decipher ack with KpubPC (qrcode[3])
-      // If ack is valid, add (username, KpubPC) to a database
-      alert(e.data);
-    };
+  const pubkey = await SecureStore.getItemAsync("pub.key")
+  const privkey = await SecureStore.getItemAsync("priv.key")
+  const calculatedhash = crypto.PBKDF2(password, salt, { hasher: crypto.algo.SHA256, keySize: 256 / 32, iterations: 10000 })
+  if (receivedhash == calculatedhash) { 
+    const key = crypto.PBKDF2(password, salt, { hasher: crypto.algo.SHA256, keySize: 3, iterations: 10000 }) // Not sure if keysize is correct
+    const message = "ID Smartphone" + " " + "Public Key Smartphone" // TODO: put a unique id here and the Pubkey
+    const cryptedMessage = crypto.AES.encrypt(message, key.toString()) // No IV { iv: iv }
+    alert("Making connection with ws://" + ip + ':8080/')
+    const ws = new WebSocket('ws://' + ip + ':8080/')
+    ws.onopen = () => {
+      ws.send(cryptedMessage.toString(crypto.enc.base64))
+      ws.onmessage = e => {
+        // Receive ack
+        // Decipher ack with KpubPC (qrcode[3])
+        // If ack is valid, add (username, KpubPC) to a database
+        alert(e.data);
+      };
+    }
   }
-  // } 
 }
